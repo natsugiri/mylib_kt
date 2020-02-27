7696ac42ad7b771bbe833a5e428be6ab3f77577a
natsugiri <natsugiri@gmail.com>
2020-02-28 04:50:04 +0900

    [Template] indent

--- Template.kt
+++ Template.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val n = nextInt()
-	val a = IntArray(n) { nextInt() }
-	a.sort()
+    val a = IntArray(n) { nextInt() }
+    a.sort()
     println(a.joinToString(" "))
 }
